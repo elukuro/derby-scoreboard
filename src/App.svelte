af48a23cc607bce1211d681852f9289e98409e7e
elukuro <heruhartantot@gmail.com>
2020-11-18 17:49:19 +0700

    Update styling

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { onMount } from 'svelte';
 	import axios from "axios";
+	import Card from './Component/Card.svelte';
 	// yay finally aync await 
 	let url = CONFIG.env.BASE_API_URL;
 	async function getData() {
@@ -25,19 +26,45 @@
 </script>
 
 <main>
-	<ul>
-		{#each challenges as challenge}
-		
-		<li>{JSON.stringify(challenge.athlete.firstname)}</li>
-		{/each}
-	</ul>
+	<div class="challenge">
+		<h1>Derby Running Distance Challenge 2021</h1>
+		<div class="result">
+			{#each challenges as challenge,index}
+				<Card challenge={challenge} index={0}/>
+				<Card challenge={challenge} index={1}/>
+				<Card challenge={challenge} index={2}/>
+				<Card challenge={challenge} index={3}/>
+				<Card challenge={challenge} index={4}/>
+				<Card challenge={challenge} index={5}/>
+				<Card challenge={challenge} index={6}/>
+				<Card challenge={challenge} index={7}/>
+				<Card challenge={challenge} index={8}/>
+			{/each}
+		</div>
+	</div>
 </main>
 
 <style lang="scss">
-	$color:red;
-	ul{
-		li{
-			color:$color;
+	.challenge{
+		background:#333;
+		h1{
+			font-size: 30px;
+			text-align: left;
+			width:100%;
+			display: block;
+			color:#555;
+			margin:0px 20px 20px 0px;
+			line-height: 1em;
+		}
+		.result{
+			padding:40px 10px;
+			background:#fff;
+			border-top-left-radius:20px;
+			border-top-right-radius:20px;
+			height:320px;
+			overflow:scroll;
+			
 		}
 	}
+	
 </style>
---
+++ src/Component/Card.svelte
@@ -0,0 +1,66 @@
+<script>
+     export let challenge;
+     export let index;
+     let workout = challenge.workout.ytd_run_totals;
+     let athlete = challenge.athlete;
+</script>
+
+<article class="cardScore">
+    <slot></slot>
+    <div class="athlete">
+        <h2>#{index +1}</h2>
+        <img src={athlete.profile} alt="profile image"/>
+        <div class="athlete-info">
+            <p><span>{athlete.firstname} {athlete.lastname}</span></p>
+            <p><span>{challenge.athlete.city}</span></p>
+        </div>
+    </div>
+    <div class="workout">
+        <p>{Math.floor(workout.distance/1000)}<span>KM</span> {workout.count}<span>Run</span> {Math.floor(workout.moving_time/60/60)}<span>Hour</span></p> 
+    </div>
+</article>
+<style lang="scss">
+	.cardScore{
+        border:1px solid #fff;
+        border-radius:3px;
+        padding:5px;
+        margin-bottom:3px;
+        box-shadow: 0px 1px 3px #ccc;
+        display: flex;
+        justify-content: space-between;
+        background:#fff;
+        .athlete{
+            display:flex;
+            align-items: center;
+            img{
+                width:40px;
+                border-radius:30px;
+            }
+            .athlete-info{
+                padding-left:10px;
+                width:95%;
+                p{
+                    color:#555;
+                    font-size: 12px;
+                    text-transform: capitalize;
+                }
+            }
+        }
+        h2{
+            font-size: 12px;
+            margin:0px 10px;
+        }
+        .workout{
+          p{
+              font-size: 11px;
+              display: inline;
+              color:#999;
+              font-weight: bold;
+              span{
+                  font-weight: normal;
+                  font-size: 8px;
+              }
+          }  
+        }
+    }
+</style>
