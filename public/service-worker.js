445874f4d1ebe89dfbc24167e9872ff1c5e7646b
elukuro <heruhartantot@gmail.com>
2020-12-07 06:10:26 +0700

    update service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,14 @@
-self.addEventListener('install', function(event) {
-    console.log('[Service workier] installing service worker', event);
-    event.waitUntil(
-        caches.open('static').then(function(cache) {
-            cache.addAll(['/', '/index.html', '/app.js', '/manifest.json']);
-        })
-    );
-})
+// self.addEventListener('install', function(event) {
+//     console.log('[Service workier] installing service worker', event);
+//     event.waitUntil(
+//         caches.open('static').then(function(cache) {
+//             cache.addAll(['/index.html', '/app.js', '/manifest.json']);
+//         })
+//     );
+// })
 
-self.addEventListener('activate', function(event) {
-    console.log('[Service Worker] Activating Service Worker ....', event);
-});
+// self.addEventListener('activate', function(event) {
+//     console.log('[Service Worker] Activating Service Worker ....', event);
+// });
+
+console.log('Hello from service worker') 
