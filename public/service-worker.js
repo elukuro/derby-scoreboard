e6b64bfd6a826f2e029fd6f1fc94b20b540fc293
elukuro <heruhartantot@gmail.com>
2020-12-09 15:57:10 +0700

    update service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -39,6 +39,11 @@ self.addEventListener('activate',async ()=>{
 
 self.addEventListener('install', function(event) {
     console.log('[Service Worker] Installing Service Worker ...', event);
+    // event.waitUntil(
+    //     caches.open('static').then(function(cache) {
+    //         cache.addAll(['/app.js', '/manifest.json']);
+    //     })
+    // );
 });
 
 self.addEventListener('push', function(event) {
@@ -55,3 +60,21 @@ const showLocalNotification = (title, body, swRegistration)=>{
     };
     swRegistration.showNotification(title,options);
 }
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else {
+          return fetch(event.request).then(function(res) {
+            return caches.open('dynamic').then(function(cache) {
+              cache.put(event.request.url, res.clone());
+              return res;
+            });
+          });
+        }
+      })
+    );
+  });
+  
