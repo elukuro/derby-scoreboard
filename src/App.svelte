fe8aa4a0540aff3178e92d3d8e8e7e98e45dd5f6
elukuro <heruhartantot@gmail.com>
2021-01-04 17:24:01 +0700

    Initialize help page

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,11 @@
 <script>
 	import Home from './Component/Home.svelte';
+	import Help from './Component/help.svelte';
 	import Router from 'svelte-spa-router';
 	// import {link} from 'svelte-spa-router'
 
 	const routes = {
+		'/help': Help,
 		'/:id?': Home,
 	}
 </script>
