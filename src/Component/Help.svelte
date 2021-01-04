fcb365a7ac52e1a258b555a286f89d41f95cf173
elukuro <heruhartantot@gmail.com>
2021-01-04 17:25:48 +0700

    add help page

---
+++ src/Component/Help.svelte
@@ -0,0 +1,34 @@
+<script>
+    
+</script>
+<main>
+    <h1>help page here</h1>
+    <div class="mode">
+        <button class="active">back to home</button>
+	</div>
+</main>
+
+<style lang="scss">
+    .mode{
+		position:fixed;
+		bottom:0px;
+		z-index:2;
+		background:#fff;
+		box-shadow: 0px 1px 3px #ccc;
+		width:100%;
+		left:0;
+		button{
+			border:none;
+			width:100%;
+			font-size: 12px;
+			padding:15px 0px;
+			color:#1488CC;
+			text-transform: uppercase;
+			font-weight:bold;
+		}
+		.active{
+			background: #1488CC;
+			color:#fff;
+		}
+	}
+</style>
